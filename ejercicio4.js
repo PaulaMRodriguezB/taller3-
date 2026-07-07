f7a5499c515e092c947028074b2b93c05d287356
Paula M Rodriguez B <rodriguezbeltranpaula94@gmail.com>
2026-07-07 10:43:09 -0500

    ejercicio 4

---
+++ ejercicio4.js
@@ -0,0 +1,23 @@
+let movimientos = [20000, 0, -15000, 0, -3200, 50000, -8500];
+
+for (let i = 0; i < movimientos.length; i++) {
+    let movimientoActual = movimientos[i];
+
+   
+    if (movimientoActual === 0) {
+        console.log("Posición " + i + ": Movimiento vacío ($0). ");
+        continue; 
+    }
+
+   
+    if (movimientoActual < 0) {
+        console.log(" Primer pago encontrado!");
+        console.log("Monto: $" + movimientoActual + "  Movimiento encontrado # " + i);
+        
+        
+        break; 
+    }
+
+    
+    console.log("Posición " + i + ": total de ingreso" + movimientoActual);
+}
